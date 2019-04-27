1922f631fbfc8d9e4301fc2eb6552799a66063d5
WalkerFifth <annuit-coeptis@hotmail.co.jp>
2019-04-27 02:18:09 -0400

    Adding another shellscript

---
+++ tablet_monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configure the tablet as a second monitor settings
+
+gtf 1280 800 60
+
+xrandr --newmode "1280x800_60.00"  83.46  1280 1344 1480 1680  800 801 804 828  -HSync +Vsync
+
+xrandr --addmode VIRTUAL1 1280x800_60.00
+
+xrandr --output VIRTUAL1 --mode 1280x800_60.00 --left-of LVDS1
+
+
